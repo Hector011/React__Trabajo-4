7a99a04c7cf9ce9c85870262c80febddd1205297
Hector011 <whiterwordgame@hotmail.com>
2023-04-07 14:11:57 -0300

    array

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -5,7 +5,7 @@ const useCrud = () => {
   const [users, setUsers] = useState();
 
   const getAllUsers = () => {
-    const URL = "http://users-crud.academlo.tech/users/";
+    const URL = "https://users-crud.academlo.tech/users/";
     axios
       .get(URL)
       .then((res) => setUsers(res.data))
@@ -13,7 +13,7 @@ const useCrud = () => {
   };
 
   const createNewUser = (data) => {
-    const URL = "http://users-crud.academlo.tech/users/";
+    const URL = "https://users-crud.academlo.tech/users/";
     axios
       .post(URL, data)
       .then(() => getAllUsers)
@@ -21,7 +21,7 @@ const useCrud = () => {
   };
 
   const deleteUserById = (id) => {
-    const URL = `http://users-crud.academlo.tech/users/${id}/`;
+    const URL = `https://users-crud.academlo.tech/users/${id}/`;
     axios
       .delete(URL)
       .then(() => getAllUsers())
@@ -29,7 +29,7 @@ const useCrud = () => {
   };
 
   const updateUserById = (id, data) => {
-    const URL = `http://users-crud.academlo.tech/users/${id}/`;
+    const URL = `https://users-crud.academlo.tech/users/${id}/`;
     axios
       .put(URL, data)
       .then(() => getAllUsers())
